c9b1de2b15e6e904ef07225c881b5a174028b5b5
joeknows718 <joeknows718@gmail.com>
2020-06-02 11:26:34 -0400

    serializers and views being worked on

--- fk12/flag/views.py
+++ fk12/flag/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from user.models import Account
 from models import Flag, Plus1, Img, Comment 
+from serializers import FlagSerializer
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
-from myapps.serializers import UserSerializer
 from rest_framework import viewsets
 from rest_framework.response import Response
 from serializers import FlagSerializer, ImgSerializer, Plus1Serializer, CommentSerializer
@@ -19,4 +19,26 @@ class FlagViewSet(viewsets.ViewSet):
 
 	def FlagDetial(self.request, pk=None):
 		queryset = Flags.objects.all()
+		flag = get_object_or_404(queryset, pk=pk)
+		#needs to query associated comments and plus 1 count and list of 
+		#+1 users 
+
+	def NewFlag(self.request):
+		#need to unpack and format to be submitted to serializer
+		#title = request.title
+		#account = request.account
+		#lon =  request.lon
+		#lat = request.lat
+		#notes =  request.notes
+		#precint =  request.precint
+		#badge_num =  request.badge_num
+		new_flag = FlagSerializer()
+		new_flag.account.update_trust()#runs model method to update trust
+		new_flag.account.trust_check()
+		return new_flag#runs model method to check trust
+
+
+
+
+
 		
